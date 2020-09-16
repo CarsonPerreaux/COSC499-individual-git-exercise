a8df89a8ef858ae06c8ab92bb7ad5b786aac5dbc
carsonperreaux <carsonperreaux@10.0.0.36>
2020-09-16 02:36:01 -0700

    Added comment for junit test for int array

--- SortedArrayDemo/src/sortedArrayDemo/SortNumberArrayTest.java
+++ SortedArrayDemo/src/sortedArrayDemo/SortNumberArrayTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 public class SortNumberArrayTest {
 	
+	//Test for the int array to see if it is working properly
 	@Test
     public void testSortMyIntArray() {
         int[] myIntArr = int[](3,1,2);
